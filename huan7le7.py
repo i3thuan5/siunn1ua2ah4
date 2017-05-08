2bc42915b0d9aabe9e6ff489f56c80a5504637a4
薛丞宏 <ihcaoe@gmail.com>
2017-05-08 17:27:31 +0800

    改傳音檔看覓

--- huan7le7.py
+++ huan7le7.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,AudioSendMessage
 )
 from siat4ting7 import YOUR_CHANNEL_SECRET, YOUR_CHANNEL_ACCESS_TOKEN
 import json
@@ -41,25 +41,33 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    資料=event.message.text
+    資料 = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=揣分詞(資料)))
+        AudioSendMessage(
+            original_content_url='https://xn--lhrz38b.xn--v0qr21b.xn--kpry57d' +
+            '/%E8%AA%9E%E9%9F%B3%E5%90%88%E6%88%90?%E6%9F%A5%E8%A9%A2%E8%85%94%E5%8F%A3=%E9%96%A9%E5%8D%97%E8%AA%9E&' +
+            '%E6%9F%A5%E8%A9%A2%E8%AA%9E%E5%8F%A5=' +
+            quote(揣分詞(資料))
+        )
+    )
+
 
 def 揣分詞(音標):
-        conn = http.client.HTTPConnection("xn--lhrz38b.xn--v0qr21b.xn--kpry57d")
-        conn.request(
-            "GET",
-            "/%E6%A8%99%E6%BC%A2%E5%AD%97%E9%9F%B3%E6%A8%99?%E6%9F%A5%E8%A9%A2%E8%85%94%E5%8F%A3=%E9%96%A9%E5%8D%97%E8%AA%9E&%E6%9F%A5%E8%A9%A2%E8%AA%9E%E5%8F%A5=" +
-            quote(音標)
-        )
-        r1 = conn.getresponse()
-        if r1.status != 200:
-            print(r1.status, r1.reason)
-            print(音標)
-            return '服務錯誤'
-        data1 = r1.read()  # This will return entire content.
-        return json.loads(data1.decode('utf-8'))['分詞']
+    conn = http.client.HTTPConnection("xn--lhrz38b.xn--v0qr21b.xn--kpry57d")
+    conn.request(
+        "GET",
+        "/%E6%A8%99%E6%BC%A2%E5%AD%97%E9%9F%B3%E6%A8%99?%E6%9F%A5%E8%A9%A2%E8%85%94%E5%8F%A3=%E9%96%A9%E5%8D%97%E8%AA%9E&%E6%9F%A5%E8%A9%A2%E8%AA%9E%E5%8F%A5=" +
+        quote(音標)
+    )
+    r1 = conn.getresponse()
+    if r1.status != 200:
+        print(r1.status, r1.reason)
+        print(音標)
+        return '服務錯誤'
+    data1 = r1.read()  # This will return entire content.
+    return json.loads(data1.decode('utf-8'))['分詞']
+
 
 if __name__ == "__main__":
     app.run()
