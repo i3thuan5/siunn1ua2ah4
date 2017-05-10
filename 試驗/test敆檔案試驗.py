468ca113bf6ec6eba6c849c98240d5284e81997b
薛丞宏 <ihcaoe@gmail.com>
2017-05-10 13:32:54 +0800

    會當敆出影片矣

--- 試驗/test敆檔案試驗.py
+++ 試驗/test敆檔案試驗.py
@@ -1,6 +1,9 @@
 from os.path import abspath, dirname, join, isfile
+from tempfile import TemporaryDirectory
 
 from django.test.testcases import TestCase
+
+
 from 鬥做伙.做影片 import 做影片
 
 
@@ -12,5 +15,29 @@ class 敆檔案試驗(TestCase):
         self.字 = join(self.目錄, 'huan1ing5.srt')
 
     def test_一組(self):
-        檔案的所在 = 做影片.敆做伙([self.圖], [self.音], [self.字])
-        self.assertTrue(isfile(檔案的所在))
+        with TemporaryDirectory() as 目錄:
+            檔案的所在 = join(目錄, 'a.avi')
+            做影片.敆做伙([self.圖], [self.音], [self.字], 檔案的所在)
+            self.assertTrue(isfile(檔案的所在))
+
+    def test_兩組(self):
+        with TemporaryDirectory() as 目錄:
+            檔案的所在 = join(目錄, 'b.avi')
+            做影片.敆做伙([self.圖, self.圖], [self.音, self.音],
+                    [self.字, self.字], 檔案的所在)
+            self.assertTrue(isfile(檔案的所在))
+
+    def test_兩組的檔案佮一組無仝(self):
+        with TemporaryDirectory() as 目錄:
+            一組檔案的所在 = join(目錄, 'c.avi')
+            做影片.敆做伙([self.圖], [self.音], [self.字], 一組檔案的所在)
+            兩組檔案的所在 = join(目錄, 'd.avi')
+            做影片.敆做伙(
+                [self.圖, self.圖],
+                [self.音, self.音],
+                [self.字, self.字],
+                兩組檔案的所在
+            )
+            with open(一組檔案的所在, 'rb') as 一組檔案:
+                with open(兩組檔案的所在, 'rb') as 兩組檔案:
+                    self.assertNotEqual(一組檔案.read(), 兩組檔案.read())
--- 鬥做伙/做影片.py
+++ 鬥做伙/做影片.py
@@ -1,6 +1,43 @@
+from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
+from os.path import join
+from tempfile import TemporaryDirectory
 
 
-class 做影片:
+class 做影片(程式腳本):
     @classmethod
-    def 敆做伙(cls, 圖陣列, 音檔陣列, 字幕陣列):
-        pass
+    def 敆做伙(cls, 圖陣列, 音檔陣列, 字幕陣列, 存檔所在):
+        with TemporaryDirectory() as 目錄:
+            全部結果檔=[]
+            for 第幾个, (圖, 音檔, 字幕) in enumerate(zip(圖陣列, 音檔陣列, 字幕陣列)):
+                暫時檔 = join(目錄, 'tmp{}.avi'.format(第幾个))
+                結果檔 = join(目錄, 'output{}.mkv'.format(第幾个))
+                cls._走指令([
+                    'avconv',
+                    '-i', 圖, '-i', 音檔,
+                    '-s', 'svga', '-y', 暫時檔,
+                ])
+                cls._走指令([
+                    'mkvmerge',
+                    '-o', 結果檔,
+                    暫時檔, 字幕,
+                ])
+                全部結果檔.append(結果檔)
+#                 mkvmerge -o 1.mkv 1-1.mkv + 1-2.mkv + 1-3.mkv
+            敆做伙結果檔 = join(目錄, 'result{}.mkv'.format(第幾个))
+            敆做伙指令 = ['mkvmerge']
+            敆做伙指令.append('-o')
+            敆做伙指令.append(敆做伙結果檔)
+            for 結果檔 in 全部結果檔:
+                敆做伙指令.append(結果檔)
+                敆做伙指令.append('+')
+            敆做伙指令.pop()
+            cls._走指令(敆做伙指令)
+            
+            上尾轉換指令 = ['avconv']
+            上尾轉換指令.append('-i')
+            上尾轉換指令.append(敆做伙結果檔)
+            上尾轉換指令.append('-c')
+            上尾轉換指令.append('copy')
+            上尾轉換指令.append('-y')
+            上尾轉換指令.append(存檔所在)
+            cls._走指令(上尾轉換指令)
