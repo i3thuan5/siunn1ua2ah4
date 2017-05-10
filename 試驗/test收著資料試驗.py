bb2352652abc8e12d8fbff208b21514b943b52b4
薛丞宏 <ihcaoe@gmail.com>
2017-05-10 14:41:46 +0800

    加前端送來資料的介面

---
+++ 試驗/test收著資料試驗.py
@@ -0,0 +1,52 @@
+from os.path import abspath, dirname, join
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+
+from django.test.testcases import TestCase
+
+from siunn1ua2ah4.settings import I7SIAT4_TOO5
+
+
+from 鬥做伙.做影片 import 做影片
+
+
+@patch('鬥做伙.做影片.做影片.敆做伙')
+class 收著資料試驗(TestCase):
+    def setUp(self):
+        self.目錄 = join(dirname(abspath(__file__)), '..', '圖')
+        self.圖 = join(self.目錄, 'ti1a2.jpg')
+        self.音 = join(self.目錄, 'huan1ing5.wav')
+        self.字 = join(self.目錄, 'huan1ing5.srt')
+
+    def test_無圖用預設圖(self, 敆做伙mock):
+        with TemporaryDirectory() as 目錄:
+            檔案的所在 = join(目錄, 'a.mp4')
+            做影片.收著資料([], [self.音], [self.字], 檔案的所在)
+            敆做伙mock.assert_called_once_with(
+                [I7SIAT4_TOO5], [self.音], [self.字], 檔案的所在)
+
+    def test_圖無夠愛循環用(self, 敆做伙mock):
+        with TemporaryDirectory() as 目錄:
+            檔案的所在 = join(目錄, 'a.mp4')
+            做影片.收著資料(
+                ['a.jpg', 'b.jpg'],
+                [self.音, self.音, self.音],
+                [self.字, self.字, self.字],
+                檔案的所在
+            )
+            敆做伙mock.assert_called_once_with(
+                ['a.jpg', 'b.jpg', 'a.jpg'],
+                [self.音, self.音, self.音],
+                [self.字, self.字, self.字],
+                檔案的所在
+            )
+
+    @patch('鬥做伙.做影片.做影片.揣聲音')
+    def test_無音用合成音(self, 揣聲音mock, 敆做伙mock):
+        揣聲音mock.return_value = self.音
+        with TemporaryDirectory() as 目錄:
+            檔案的所在 = join(目錄, 'a.mp4')
+            做影片.收著資料([self.圖], [], [self.字], 檔案的所在)
+            敆做伙mock.assert_called_once_with(
+                [self.圖], [self.音], [self.字], 檔案的所在
+            )
--- 鬥做伙/做影片.py
+++ 鬥做伙/做影片.py
@@ -1,18 +1,36 @@
 from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
 from os.path import join
 from tempfile import TemporaryDirectory
+from itertools import zip_longest
+from siunn1ua2ah4.settings import I7SIAT4_TOO5
 
 
 class 做影片(程式腳本):
     @classmethod
-    def 敆做伙(cls, 圖陣列, 音檔陣列, 字幕陣列, 存檔所在):
+    def 收著資料(cls, 圖陣列, 聲陣列, 字陣列, 存檔所在):
+        新聲陣列 = []
+        for 聲, 文本 in zip_longest(聲陣列, 字陣列):
+            if 聲 is not None:
+                新聲陣列.append(聲)
+            else:
+                新聲陣列.append(cls.揣聲音(文本))
+        if len(圖陣列) == 0:
+            新圖陣列 = [I7SIAT4_TOO5] * len(字陣列)
+        else:
+            新圖陣列 = []
+            while len(新圖陣列) < len(字陣列):
+                新圖陣列.append(圖陣列[len(新圖陣列) % len(圖陣列)])
+        cls.敆做伙(新圖陣列, 新聲陣列, 字陣列, 存檔所在)
+
+    @classmethod
+    def 敆做伙(cls, 圖陣列, 聲陣列, 字陣列, 存檔所在):
         with TemporaryDirectory() as 目錄:
             全部結果檔 = []
-            for 第幾个, (圖, 音檔, 字幕) in enumerate(zip(圖陣列, 音檔陣列, 字幕陣列)):
+            for 第幾个, (圖, 聲, 字) in enumerate(zip(圖陣列, 聲陣列, 字陣列)):
                 結果檔 = join(目錄, 'output{}.mkv'.format(第幾个))
                 cls._走指令([
                     'avconv',
-                    '-i', 圖, '-i', 音檔, '-vf', 'subtitles={}'.format(字幕),
+                    '-i', 圖, '-i', 聲, '-vf', 'subtitles={}'.format(字),
                     '-s', 'svga', '-y', 結果檔,
                 ])
                 全部結果檔.append(結果檔)
@@ -34,3 +52,7 @@ class 做影片(程式腳本):
             上尾轉換指令.append('-y')
             上尾轉換指令.append(存檔所在)
             cls._走指令(上尾轉換指令)
+
+    @classmethod
+    def 揣聲音(cls, 字):
+        pass
