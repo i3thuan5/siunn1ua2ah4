97a928e4a4b1d9e78982dc37944ba4854bd7202f
root <root@ubuntu.members.linode.com>
2017-05-10 01:27:08 +0000

    莫限網域

--- siunn1ua2ah4/settings.py
+++ siunn1ua2ah4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gflv0!2&7iz&mo(=3qa&07!trgj)-=5e75+gwbt#&s95!tgpo6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
