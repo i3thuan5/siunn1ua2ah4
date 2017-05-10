3d85606388e11259b37b305b2df477b601829ec8
薛丞宏 <ihcaoe@gmail.com>
2017-05-10 15:41:40 +0800

    id分類

--- line回應/models.py
+++ line回應/models.py
@@ -7,10 +7,19 @@ class 圖片表(models.Model):
     檔案 = models.FileField()
 
     @classmethod
+    def _提著編號(cls, source):
+        if source.type == 'user':
+            return source.user_id
+        if source.type == 'group':
+            return source.group_id
+#         if source.type=='room'
+        return source.room_id
+
+    @classmethod
     def 加一張圖(cls, source, content):
-        圖 = cls.objects.create(使用者類型=source.type, 使用者編號=source.id)
+        圖 = cls.objects.create(使用者類型=source.type, 使用者編號=cls._提著編號(source))
         圖.檔案.save('圖', b''.join(content))
 
     @classmethod
     def 全部圖(cls, source, message):
-        return cls.objects.filter(使用者類型=source.type, 使用者編號=source.id)
+        return cls.objects.filter(使用者類型=source.type, 使用者編號=cls._提著編號(source))
