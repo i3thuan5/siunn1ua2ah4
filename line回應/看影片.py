38a26a8cea1747691a69d38b7f554ed8e40ea2de
fafoy <ihcaoe@gmail.com>
2017-05-11 11:05:42 +0800

    整理文案

--- line回應/看影片.py
+++ line回應/看影片.py
@@ -4,5 +4,13 @@ from django.http.response import HttpResponse
 
 def 影片連結(request, path):
     return HttpResponse(
-        "<a href='/upload/{}' download>ss</a>".format(path)
+        """
+<doctype html>
+<body>
+<a href='/upload/{}' download>下載影片</a><br/>
+若無法度掠影片，請試正爿面頂目錄「以其他應用程式開啟」的chrome開。<br/>
+掠落來就會當分享矣
+</body>
+"""
+        .format(path)
     )
