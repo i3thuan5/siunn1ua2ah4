071a2a903b14650652923631d9b10125c82a2f52
fafoy <ihcaoe@gmail.com>
2017-05-11 11:21:52 +0800

    檔名攏號無仝好矣

--- line回應/看影片.py
+++ line回應/看影片.py
@@ -16,7 +16,7 @@ def 影片連結(request, path):
 <body>
 <h1>傷倚矣 - 生影片</h1>
 <p>
-    <a href='/upload/{}' download='siunn7ua2ah4.mp4'>下載影片</a>
+    <a href='/upload/{0}' download='{0}.mp4'>下載影片</a>
 </p>
 <p>
 若出現「不支援檔案下載功能，請透過其他瀏覽器再試一次」，請試「以其他應用程式開啟」的chrome開。<br/>
