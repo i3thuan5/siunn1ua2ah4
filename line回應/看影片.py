fca70ba7c7998458cbac51e72c8ef24ea5b9808a
fafoy <ihcaoe@gmail.com>
2017-05-11 11:13:43 +0800

    改掠的檔名，手機仔才有法度開

--- line回應/看影片.py
+++ line回應/看影片.py
@@ -16,10 +16,10 @@ def 影片連結(request, path):
 <body>
 <h1>傷倚矣 - 生影片</h1>
 <p>
-    <a href='/upload/{}' download>下載影片</a>
+    <a href='/upload/{}' download='siunn7ua2ah4.mp4'>下載影片</a>
 </p>
 <p>
-若出現「不支援檔案下載功能」，無法度掠影片，請試正爿面頂目錄「以其他應用程式開啟」的chrome開。<br/>
+若出現「不支援檔案下載功能，請透過其他瀏覽器再試一次」，請試「以其他應用程式開啟」的chrome開。<br/>
 </p>
 <p>
 掠落來就會當分享到fb佮line矣
